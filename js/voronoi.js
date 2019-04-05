724cec2db8135884b694902c2001d72fbe233e13
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2019-04-05 11:28:20 -0700

    Refactor voronoi generation Add exports for reuse

--- js/voronoi.js
+++ js/voronoi.js
@@ -1,103 +1,54 @@
 const fs = require('fs');
 
+const d3 = require('d3-delaunay');
 const turf = require('@turf/turf');
 
-let cities = JSON.parse(
-  fs.readFileSync(__dirname + '/../.data/cities.geojson')
-);
-let segments = JSON.parse(
-  fs.readFileSync(
-    __dirname + '/../.data/combined_filtered_segment_midpoints.geojson'
-  )
-);
+const readJsonFile = function (fileName) {
+  return JSON.parse(
+    fs.readFileSync(__dirname + fileName)
+  );
+}
 
-const bbox = turf.bbox(cities);
+const generateVoronoi = function ({ outputFile, inputFile, bbox }) {
+  let features = readJsonFile(inputFile);
 
-const streetSegmentTypeMap = {
-  '1110': 'Freeway',
-  '1120': 'Ramps, Interchanges(one Freeway to another)',
-  '1121':
-    'On - Ramp or Slip Road Linkage(used to enter a Freeway or lower class road)',
-  '1122':
-    'Off - Ramp or Slip Road Linkage(used to exit a Freeway or lower class road)',
-  '1123':
-    'On / Off - Ramp or Slip Road Linkage(used to enter or exit a Freeway or lower class road)',
-  '1200': 'Highway',
-  '1221':
-    'On - Ramp or Slip Road Linkage(used to enter a Highway or lower class road)',
-  '1222':
-    'Off - Ramp or Slip Road Linkage(used to exit a Highway or lower class road)',
-  '1223':
-    'On / Off - Ramp or Slip Road Linkage(used to enter or exit a Highway or lower class road)',
-  '1300': 'Primary Arterial',
-  '1321':
-    'Primary Arterial Linkage(ramp/ slip road used to enter or exit a Primary Arterial or loser class road)',
-  '1400': 'Arterial',
-  '1421':
-    'Arterial Linkage(ramp / slip road used to enter or exit an Arterial or lower class road)',
-  '1450': 'Tertiary Street - Neighborhood Collector',
-  '1471':
-    'Tertiary Street Linkage(ramp / slip road used to enter a Tertiary Street or lower class road)',
-  '1500': 'Minor Residential',
-  '1521': 'Ramp / Slip Road used to enter or exit a Residential Street',
-  '1550':
-    "Minor Residential Street(Unclassified), named but without addresses(Clackamas Co.only, SUBAREA = 'C')",
-  '1560':
-    "Minor Residential Street(Unclassified), unnamed and without addresses(Clackamas Co.only, SUBAREA = 'C')",
-  '1600': 'Alley',
-  '1700': 'Private Named Road',
-  '1780': 'Private Street, (planned)',
-  '1800': 'Unnamed Private Road / Driveway',
-  '1900': 'Pulic ROW EXISTS but no built roadway exists',
-  '1950': 'Public planned street with addresses(will be active soon)',
-  '1970': 'Virtual Segment; not a real roadway(accommodates address anomalies)',
-  '1980':
-    'Paper Street with addresses(accommodates address anomalies - in ROW unlike 1970)',
-  '1990': 'Vacated Street',
-  '2100': 'Railroad',
-  '2200': 'Rapid Transit(MAX)',
-  '3100': 'Non - Specific or Shared - Use Path',
-  '3230': 'Steps(most have addresses)',
-  '5101':
-    'Freeway with Rapid Transit(MAX or streetcar).Auto traffic and rail trackage in shared ROW.',
-  '5201':
-    'Highway with Rapid Transit(MAX or streetcar).Auto traffic and rail trackage in shared ROW.',
-  '5301':
-    'Primary Arterial with Rapid Transit(MAX or streetcar).Auto traffic and rail trackage in shared ROW.',
-  '5401':
-    'Arterial with Rapid Transit(MAX or streetcar).Auto traffic and rail trackage in shared ROW.',
-  '5402':
-    'Secondary arterial with rapid transit, but no regular vehicle traffic.',
-  '5450':
-    'Tertiary Street with Railroad.Auto traffic and rail trackage in shared ROW.',
-  '5451':
-    'Tertiary Street with Rapid Transit(MAX or streetcar).Auto traffic and rail trackage in shared ROW.',
-  '5500':
-    'Minor Residential with Railroad.Auto traffic and rail trackage in shared ROW.',
-  '5501':
-    'Minor Residential with Rapid Transit(MAX or streetcar).Auto traffic and rail trackage in shared ROW.',
-  '7700':
-    'Short segments indicating mile post locations along certain highways to assist emergency dispatch in Clackamas and Washington Counties.These are short pseudo - segments that do not exist(Clackamas and Washington Counties only).',
-  '8224': "Unknown Type(only in Yamhill County, SUBAREA = 'Y')",
-  '9000': 'Forest Service Road'
-};
+  let points = features.features.reduce((prev, curr) => {
+    prev.push(curr.geometry.coordinates);
+    return prev;
+  }, []);
 
-let points = segments.features.reduce((prev, curr, idx, arr) => {
-  prev.push(curr.geometry.coordinates);
+  let delaunay = d3.Delaunay.from(points);
+  let voronoi = delaunay.voronoi(bbox);
+
+  features = [];
+  for (const polygon of voronoi.cellPolygons()) {
+    features.push(turf.polygon([polygon]));
+  }
+
+  fs.writeFileSync(
+    __dirname + outputFile,
+    JSON.stringify(turf.featureCollection(features))
+  );
+}
+
+const cities = readJsonFile('/../.data/cities.geojson');
+let bbox = turf.bbox(cities);
+
+
+generateVoronoi({ inputFile: '/../.data/combined_filtered_segment_midpoints.geojson', outputFile: '/../.data/segments_voronoi.geojson', bbox: bbox });
+
+cities.features = cities.features.reduce((prev, curr) => {
+  if (curr.properties.CITYNAME == 'Portland') {
+    prev.push(curr);
+  }
   return prev;
 }, []);
 
-const d3 = require('d3-delaunay');
+bbox = turf.bbox(cities);
 
-let delaunay = d3.Delaunay.from(points);
-let voronoi = delaunay.voronoi(bbox);
+generateVoronoi({ inputFile: '/../.data/mesh_centroid.geojson', outputFile: '/../.data/mesh_voronoi.geojson', bbox: bbox });
 
-let features = [];
-for (const polygon of voronoi.cellPolygons()) {
-  features.push(turf.polygon([polygon]));
+module.exports = {
+  readJsonFile,
+  generateVoronoi
 }
-
-fs.writeFileSync(
-  __dirname + '/../.data/segments_voronoi.geojson',
-  JSON.stringify(turf.featureCollection(features))
-);
